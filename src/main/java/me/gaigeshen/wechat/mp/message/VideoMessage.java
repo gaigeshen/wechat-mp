7d4e42ae944b862ee28dd7bb516287809328cf36
gaigeshen <gaigeshen@qq.com>
2019-04-26 17:03:56 +0800

    语音消息增加识别参数

--- src/main/java/me/gaigeshen/wechat/mp/message/VideoMessage.java
+++ src/main/java/me/gaigeshen/wechat/mp/message/VideoMessage.java
@@ -6,11 +6,15 @@ package me.gaigeshen.wechat.mp.message;
 public class VideoMessage extends AbstractMessage {
   private String mediaId;
   private String thumbMediaId;
+  private String recognition;
 
   public String getMediaId() {
     return mediaId;
   }
   public String getThumbMediaId() {
     return thumbMediaId;
+  }
+  public String getRecognition() {
+    return recognition;
   }
 }
