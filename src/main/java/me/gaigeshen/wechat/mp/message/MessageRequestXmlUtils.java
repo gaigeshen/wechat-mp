5d9a898efc82bff0bec346b719e02c13040b6521
gaigeshen <gaigeshen@qq.com>
2019-05-06 10:58:27 +0800

    解决消息请求体转换消息类型对象的错误，获取不到任何节点

--- src/main/java/me/gaigeshen/wechat/mp/message/MessageRequestXmlUtils.java
+++ src/main/java/me/gaigeshen/wechat/mp/message/MessageRequestXmlUtils.java
@@ -1,6 +1,7 @@
 package me.gaigeshen.wechat.mp.message;
 
 import org.apache.commons.lang3.ClassUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.dom4j.Document;
 import org.dom4j.Element;
@@ -17,11 +18,11 @@ final class MessageRequestXmlUtils {
 
   /**
    *
-   * @param document 文档内容
+   * @param document 消息文档内容
    * @param targetType 指定类型
    * @return 指定的类型的对象
    */
-  static <R> R parseToObject(Document document, Class<R> targetType) {
+  static <R extends Message> R parseToObject(Document document, Class<R> targetType) {
     R response;
     try {
       response = targetType.newInstance();
@@ -31,7 +32,8 @@ final class MessageRequestXmlUtils {
     Field[] fields = FieldUtils.getAllFields(targetType);
     for (Field field : fields) {
       field.setAccessible(true);
-      Element node = (Element) document.selectSingleNode("//" + field.getName());
+      String fieldName = field.getName();
+      Element node = (Element) document.selectSingleNode("//" + StringUtils.left(fieldName, 1) + fieldName.substring(1));
       if (node == null) {
         continue; // 可能不存在该字段
       }
