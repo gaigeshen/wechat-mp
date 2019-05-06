7fe9d70c0251ca9d42642a4708b1a9fcf68cb51c
gaigeshen <gaigeshen@qq.com>
2019-05-06 14:18:16 +0800

    地理位置消息类添加缺失的方法

--- src/main/java/me/gaigeshen/wechat/mp/message/LocationMessage.java
+++ src/main/java/me/gaigeshen/wechat/mp/message/LocationMessage.java
@@ -8,4 +8,17 @@ public class LocationMessage extends AbstractMessage {
   private Double location_Y;
   private Integer scale;
   private String label;
+
+  public Double getLocation_X() {
+    return location_X;
+  }
+  public Double getLocation_Y() {
+    return location_Y;
+  }
+  public Integer getScale() {
+    return scale;
+  }
+  public String getLabel() {
+    return label;
+  }
 }
